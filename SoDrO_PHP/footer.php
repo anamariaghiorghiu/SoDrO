72fb20dacca19ab35e21fe592974ff22a7619c0b
Adina Nedelcu <adina_andreea_nedelcu@yahoo.com>
2022-06-22 00:39:02 +0300

    Footer ->  fixed responsiveness + validation

--- SoDrO_PHP/footer.php
+++ SoDrO_PHP/footer.php
@@ -1,22 +1,22 @@
 	<div class="botnav">
-		<nav>
-		<button class=button>
-			<img src="images/Products_logo.png" class="logo-botnav">
-			<a href="products.php">Products</a>
-		</button>
-		<button class=button>
-			<img src="images/AboutUs_logo.png" class="logo-botnav">
-			<a class="current" href="aboutUs.php">About Us</a>
-		</button>
-		<button class=button>
-			<img src="images/Terms_and_conditions_logo.png" class="logo-botnav">
-			<a href="termsAndConditions.php">Terms and Conditions</a>
-		</button>
-		<button class=button>
-			<img src="images/Preferences_logo.png" class="logo-botnav">
-			<a href="preferences.php">Preferences</a>
-		</button>
-		</nav>
+		<ul class="nav">
+			<li>
+				<img src="images/Products_logo.png" alt="Products logo" class="logo-botnav">
+				<a href="products.php">Products</a>
+			</li>
+			<li>
+				<img src="images/AboutUs_logo.png" alt="About Us logo" class="logo-botnav">
+				<a class="current" href="aboutUs.php">About Us</a>
+			</li>
+			<li>
+				<img src="images/Terms_and_conditions_logo.png" alt="Terms and conditions logo" class="logo-botnav">
+				<a href="termsAndConditions.php">Terms and Conditions</a>
+			</li>
+			<li>
+				<img src="images/Preferences_logo.png" alt="Preferences logo" class="logo-botnav">
+				<a href="preferences.php">Preferences</a>
+			</li>
+		</ul>
 	</div>
 </div>
 </body>
