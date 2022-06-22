e138e16979aa680d65fc5f8cf580f32e88abd485
anamariaghiorghiu <ana.maria.ghiorghiu10@gmail.com>
2022-06-22 05:01:46 +0300

    update

--- SoDrO_PHP/header.php
+++ SoDrO_PHP/header.php
@@ -3,34 +3,45 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang ="en">
 <head>
 	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>SoDrO</title>
 	<link rel="stylesheet" href="style.css">
 	<title></title>
 </head>
+
 <body>
-<div class="container">
-	<div class="topnav">
-		<img src="images/logo.png" class="logo">
-		<h1>SoDrO</h1>
-		<nav>
-			<ul>
-				<li><a href="startPage.php">Home</a></li>
-				<?php
-				if (isset($_SESSION["userid"])){
-					echo "<li><a href='profile.php'>Profile</a></li>";
-					echo "<li><a href='includes/logout-inc.php'>Log out</a></li>";
-				}
-				else {
-					echo "<li><a href='register.php'>Register</a></li>";
-					echo "<li><a href='login.php'>Log in</a></li>";
-				}
-				?>
-			</ul>
-		</nav>
-		<img src="images/userpic_logo.png" class="logo_user">
-	</div>
-	
+
+<section id="header">
+        <div id = "navbar_start">
+            <img src="images/logo.png" class="logo" alt="logo">
+            <h1>SoDrO</h1>
+        </div>
+        <div>
+            <ul id="navbar">
+		<li><a href="startPage.php">Home</a></li>
+		<?php
+		if (isset($_SESSION["userid"])){
+                	echo "<li><a href='profile.php'>Profile</a></li>";
+                	echo "<li><a href='includes/logout-inc.php'>Logout</a></li>";
+		}else{
+			echo "<li><a href='register.php'>Register</a></li>";
+			echo "<li><a  href='login.php'>Log in</a></li>";
+			echo "<li><a href='profile.php'><img src='images/userpic_logo.png' class='userpiclogo' alt='userpiclogo'></a></li>";
+		}
+        ?>     
+            </ul>
+        </div>
+        
+        <div id="mobile">
+            <img src="images/close.png" id="close" class="close_size" alt="close">
+            <img src="images/bar.png" id="bar" class="bar_size" alt="bar">
+        </div>
+    </section>
+    
+    <script src="script.js"></script>
+</body>
+</html>
--- SoDrO_PHP/startPage.php
+++ SoDrO_PHP/startPage.php
@@ -1,27 +1,46 @@
 <!DOCTYPE html>
 <html lang = "en">
-	<title>Start Page</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="style.css">
 
-	<body>
-		<div class="container">
-			<div class="topnav">
-				<img src="images/logo.png" alt="logo" class="logo">
-				<h1>SoDrO</h1>
-			</div>
-			<div class="startPageContent">
-				<h2>Soft Drinks Organizer</h2>
-				<h3>a sip of ingeniosity.</h3>
-				<img src="images/Pahar.png" alt="pahar" class="pahar">
-				<form action="register.php">
-					<button class = "block"> 
-							I am <i>thirsty</i>. <br>
-							Let's get started!
-					</button>
-				</form>
-			</div>
-		</div>
-	</body>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SoDrO</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <section id="header_start">
+        <a>
+            <img src="images/logo.png" class="logo">
+        </a>
+
+        <div>
+            <ul id="navbar_start">
+                <h1>SoDrO</h1>
+            </ul>
+        </div>
+    </section>
+
+    <section id="content_start">
+        <h2>Soft Drinks Organizer</h2>
+        <h3>a sip of ingeniosity.</h3>
+
+        <form action="register.php">
+            <div class="buton">
+                <button class = "block"> 
+                    I am <i>thirsty</i>. <br> Let's get started!
+                </button>
+            </div>
+        </form>
+        
+        <div class ="pahar">
+            <img src="images/Pahar.png" alt="pahar" class="pahar">
+        </div>
+                
+    </section>
+    
+    <script src="script.js"></script>
+</body>
+
 </html>
