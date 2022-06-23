a38f65346bdd7c3fd2d0a6dcb84fde229be7fc08
Adina Nedelcu <adina_andreea_nedelcu@yahoo.com>
2022-06-23 03:11:39 +0300

    Lists -> validation + responsiveness + design

---
+++ SoDrO_PHP/lists.php
@@ -0,0 +1,63 @@
+<?php
+ include_once 'header.php';
+?>
+
+<div class="lists-content">
+    <h2>Soft Drinks Organizer</h2>
+    <h3>a sip of ingeniosity.</h3>
+    <button class="add-list"> Add new list </button>
+    <table class="lists"> 
+        <tr>
+            <td> 1 </td>
+            <td> Adina's Birthday </td>
+            <td> <button class="view"> View </button> </td>
+            <td> <button class="remove"> Remove </button> </td>
+        </tr>
+        <tr> 
+            <td> 2 </td>
+            <td> Cleo's Birthday </td>
+            <td> <button class="view"> View </button> </td>
+            <td> <button class="remove"> Remove </button> </td>
+        </tr>
+        <tr> 
+            <td> 3 </td>
+            <td> Mariuca's Party </td>
+            <td> <button class="view"> View </button> </td>
+            <td> <button class="remove"> Remove </button> </td>
+        </tr>
+        <tr> 
+            <td> 4 </td>
+            <td> Sami's surprise party </td>
+            <td> <button class="view"> View </button> </td>
+            <td> <button class="remove"> Remove </button> </td>
+        </tr>
+        <tr> 
+            <td> 5 </td>
+            <td> Dinner at Cezar </td>
+            <td> <button class="view"> View </button> </td>
+            <td> <button class="remove"> Remove </button> </td>
+        </tr>
+        <tr> 
+            <td> 6 </td>
+            <td> N-am alte idei no.1 </td>
+            <td> <button class="view"> View </button> </td>
+            <td> <button class="remove"> Remove </button> </td>
+        </tr>
+        <tr> 
+            <td> 7 </td>
+            <td> N-am alte idei no.2 </td>
+            <td> <button class="view"> View </button> </td>
+            <td> <button class="remove"> Remove </button> </td>
+        </tr>
+        <tr> 
+            <td> 8 </td>
+            <td> N-am alte idei no.3 </td>
+            <td> <button class="view"> View </button> </td>
+            <td> <button class="remove"> Remove </button> </td>
+        </tr>
+    </table>
+</div>
+
+<?php
+ include_once 'footer.php';
+?>
