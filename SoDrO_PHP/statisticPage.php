14303c0d69fa6364e420c177b2c8eeaada8b4f6b
anamariaghiorghiu <ana.maria.ghiorghiu10@gmail.com>
2022-06-25 00:28:30 +0300

    statistics- just the start

---
+++ SoDrO_PHP/statisticPage.php
@@ -0,0 +1,50 @@
+<?php
+ include_once 'header.php';
+?>
+
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>SoDrO</title>
+	<link rel="stylesheet" href="style.css">
+	<title></title>
+	<script src="jquery.js"></script>
+
+<body>
+    <div class="statisticContent">
+        <h2>Soft Drinks Organizer</h2>
+        <h3>a sip of ingeniosity.</h3>
+        <div class="background">
+            <div class="pie-charts">
+                <div class="pieID--micro-skills pie-chart-wrapper">
+                    <p>Titlu de statistica</p>
+                    <div class="pie-chart">
+                        <div class="pie-chart__pie">
+                            
+                        </div>
+                        <ul class="pie-chart__legend">
+                            <li>stats_legend_item_numero3<span>1</span></li>
+                            <li>stats_legend_item_numero3<span>1</span></li>
+                            <li>stats_legend_item_numero3<span>2</span></li>
+                            <li>stats_legend_item_numero3<span>2.2</span></li>
+                            <li>stats_legend_item_numero3<span>0.8</span></li>
+                            <li>stats_legend_item_numero3<span>0.7</span></li>
+                            <li>stats_legend_item_numero3<span>2.3</span></li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    <script src="chart.js"></script>
+</body>
+</html>
+
+<?php
+ include_once 'footer.php';
+?>
